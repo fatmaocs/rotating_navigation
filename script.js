a603fe9a72ee4160cd647e6d19afd76a9cffe021
unknown <fatmaocs2016@gmail.com>
2021-04-01 14:26:54 +0200

    add all files

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+
+let close_button = document.getElementById('close');
+let open_button = document.getElementById('open');
+let container = document.querySelector('.container');
+
+
+open_button.addEventListener('click', () => {
+    container.classList.add('show_nav');
+});
+
+close_button.addEventListener('click', () => {
+
+    container.classList.remove('show_nav');
+});
